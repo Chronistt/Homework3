803a544bd839bef0f89d81519da28995f6f960cd
norki <norkink89@mail.ru>
2022-11-28 13:35:20 +0400

    Tasks 4-6

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,35 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        dog = dog + 4;
+        dog = dog + 4; // Пишем код для задачи 2
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        dog = dog - 3.5;
+        dog = dog - 3.5; // Пишем код для задачи 3
         cat = cat - 1.6;
         paper = paper + 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
 
+        System.out.println("Задача 4");
+        var friend = 19; // Пишем код для задачи 4
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("Задача 5");
+        var frog = 3.5; // Пишем код для задачи 5
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
