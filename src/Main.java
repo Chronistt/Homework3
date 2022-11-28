656c34c368f400bd385e21febe848441fb3b2089
norki <norkink89@mail.ru>
2022-11-28 15:31:17 +0400

    Tasks 1-8

--- src/Main.java
+++ src/Main.java
@@ -40,5 +40,30 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+        System.out.println("Задача 6");
+        var boxer1Weight = 78.2; // Пишем код для задачи 6
+        var boxer2Weight = 82.7;
+        var commonWeight = boxer1Weight + boxer2Weight;
+        System.out.println("Общий вес боксеров " + commonWeight + " кг ");
+
+        var differenceWeight = boxer2Weight - boxer1Weight;
+        System.out.println("Разница в весе межде боксерами " + differenceWeight + " кг");
+
+        System.out.println("Задача 7");
+        var difference2Weight = boxer2Weight % boxer1Weight; // Пишем код для задачи 7
+        System.out.println("Разница в весе межде боксерами " + difference2Weight + " кг");
+
+        System.out.println("Задача 8");
+        var workInitCommonHours = 640; // Пишем код для задачи 8
+        var workPersonalHours = 8;
+        var personel = workInitCommonHours / workPersonalHours;
+        System.out.println("Всего работников в компании — " + personel + " человек");
+
+        var addedPersonel = personel + 94;
+        var workFinalCommonHours = addedPersonel * 8;
+        System.out.println("Если в компании работает " + addedPersonel + " человек, то всего " + workFinalCommonHours + " часов работы может быть поделено между сотрудниками");
+
+
     }
 }
